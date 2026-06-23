cee4c45b5e2e8b07fb00bb312511a10ac45f4ee9
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2026-06-23 12:40:47 -0700

    feat: add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { holocron } from '@holocron.so/vite'
 
 export default defineConfig({
   clearScreen: false,
+  base: process.env.GH_PAGES ? '/wiki-holocron-template/' : undefined,
   plugins: [holocron({ pagesDir: './src' })],
 })
