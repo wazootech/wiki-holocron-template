979f2207abcde1ffbd1ac70ef5abe5b0b8e53fa5
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2026-06-23 13:57:09 -0700

    fix: use holocron deploy for GH Pages build with prerendering

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import { holocron } from '@holocron.so/vite'
 
 export default defineConfig({
   clearScreen: false,
-  base: process.env.GH_PAGES ? '/wiki-holocron-template/' : undefined,
   plugins: [holocron({ pagesDir: './src' })],
 })
